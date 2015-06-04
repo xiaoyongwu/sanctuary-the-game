1ba74ed87f8030a36afc8728e34d3a07529c92d3
uriel bertoche <urielbertoche@uriels-iMac.local>
2015-06-04 18:16:22 -0300

    added camera

--- Sanctuary/GameScene.swift
+++ Sanctuary/GameScene.swift
@@ -9,73 +9,11 @@
 import SpriteKit
 
 class GameScene: MapScene {
-    override var mapName : String {
-        get {
-            return mapName
-        }
-        set {
-            if newValue {
-                
-            }
-        }
-    }
     
     override func didMoveToView(view: SKView) {
+        self.mapName = "town.tmx"
         game = Game(view: view, scene: self)
         /* Setup your scene here */
         setupScene()
-    }
-    
-    func setupScene() {
-        backgroundColor = UIColor(red: 165.0/255.0, green: 216.0/255.0, blue: 255.0/255.0, alpha: 1.0)
-        
-        physicsWorld.gravity = CGVectorMake(0, -9.8)
-        physicsWorld.contactDelegate = self
-        
-        anchorPoint = CGPoint(x: 0, y: 0)
-        position = CGPoint(x: 0, y: 0)
-        
-        let point = tileMap.calculateAccumulatedFrame()
-        let layer_meta = tileMap.layerNamed("meta")
-        // layer_meta.hidden = true
-
-        tileMap.position = CGPoint(x: 0, y: 0)
-        addChild(tileMap)
-        
-        addFloor()
-    }
-    
-    func addFloor() {
-        for var a = 0; a < Int(tileMap.mapSize.width); a++ {
-            for var b = 0; b < Int(tileMap.mapSize.height); b++ {
-                let layerInfo:TMXLayerInfo = tileMap.layers.firstObject as! TMXLayerInfo
-                let point = CGPoint(x: a, y: b)
-                //gid is the order of the tiles in Tiled starting at 1 (wierddd)
-                let gid = layerInfo.layer.tileGidAt(layerInfo.layer.pointForCoord(point))
-                
-                if gid == 2 || gid == 9 || gid == 8{
-                    let node = layerInfo.layer.tileAtCoord(point)
-                    node.physicsBody = SKPhysicsBody(rectangleOfSize: node.frame.size)
-                    node.physicsBody?.dynamic = false
-                }
-            }
-        }
-    }
-    
-    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
-        let touch = touches.first as! UITouch
-        let location = touch.locationInNode(self)
-        
-        let meta_layer = tileMap.layerNamed("meta")
-        let coord_touch = meta_layer.coordForPoint(location)
-        let tile = meta_layer.layerInfo.tileGidAtCoord(coord_touch)
-        
-        let alert_view = UIAlertView(title: "Tile GID", message: "\(tile)", delegate: nil, cancelButtonTitle: "Close")
-        
-        alert_view.show()
-    }
-   
-    override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */
     }
 }
--- Sanctuary/MapScene.swift
+++ Sanctuary/MapScene.swift
@@ -12,12 +12,13 @@ class MapScene: SKScene, SKPhysicsContactDelegate {
     var lastMenuPosition = 0
     var game : Game?
     
-    var mapName = "town.tmx"
-    var tileMap:JSTileMap! //= JSTileMap(named: "town.tmx")
+    var mapName = ""
+    var map:JSTileMap! //= JSTileMap(named: "town.tmx")
     var tileSize:CGSize!
+    var camera:SKSpriteNode!
     
     func setMap () {
-        tileMap = JSTileMap(named: mapName)
+        map = JSTileMap(named: mapName)
     }
     
     override func didMoveToView(view: SKView) {
@@ -30,23 +31,39 @@ class MapScene: SKScene, SKPhysicsContactDelegate {
         setMap()
         backgroundColor = UIColor(red: 165.0/255.0, green: 216.0/255.0, blue: 255.0/255.0, alpha: 1.0)
         
+        self.camera = SKSpriteNode(color: SKColor.blackColor(), size: CGSizeMake(5, 5))
+        self.camera.position = CGPointMake(size.width * 0.5, size.height * 0.5)
+        self.map.addChild(camera)
+        
         anchorPoint = CGPoint(x: 0, y: 0)
         position = CGPoint(x: 0, y: 0)
         
-        let point = tileMap.calculateAccumulatedFrame()
-        let layer_meta = tileMap.layerNamed("meta")
+        let point = map.calculateAccumulatedFrame()
+        let layer_meta = map.layerNamed("meta")
         layer_meta.hidden = true
         
-        tileMap.position = CGPoint(x: 0, y: 0)
-        addChild(tileMap)
+        map.position = CGPoint(x: 0, y: 0)
+        addChild(map)
         
         addFloor()
     }
     
+    override func touchesMoved(touches: Set<NSObject>, withEvent event: UIEvent) {
+        for touch : AnyObject in touches {
+            var touchLocation = touch.locationInNode(self)
+            var previousTouchLocation = touch.previousLocationInNode(self)
+            var movement = CGPointMake(touchLocation.x - previousTouchLocation.x,
+                                       touchLocation.y - previousTouchLocation.y)
+            
+            self.camera.position = CGPointMake(self.camera.position.x + movement.x,
+                                               self.camera.position.y + movement.y)
+        }
+    }
+    
     func addFloor() {
-        for var a = 0; a < Int(tileMap.mapSize.width); a++ {
-            for var b = 0; b < Int(tileMap.mapSize.height); b++ {
-                let layerInfo:TMXLayerInfo = tileMap.layers.firstObject as! TMXLayerInfo
+        for var a = 0; a < Int(map.mapSize.width); a++ {
+            for var b = 0; b < Int(map.mapSize.height); b++ {
+                let layerInfo:TMXLayerInfo = map.layers.firstObject as! TMXLayerInfo
                 let point = CGPoint(x: a, y: b)
                 //gid is the order of the tiles in Tiled starting at 1 (wierddd)
                 let gid = layerInfo.layer.tileGidAt(layerInfo.layer.pointForCoord(point))
@@ -60,7 +77,15 @@ class MapScene: SKScene, SKPhysicsContactDelegate {
         }
     }
     
+    func updateView() {
+        
+        // Center veiw on position of camera in the world
+        self.map.position = CGPointMake((self.size.width * 0.5 - self.camera.position.x),
+                                            (self.size.height * 0.5 - self.camera.position.y))
+    }
+    
     override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        /*
         let touch = touches.first as! UITouch
         let location = touch.locationInNode(self)
         
@@ -70,10 +95,12 @@ class MapScene: SKScene, SKPhysicsContactDelegate {
         
         let alert_view = UIAlertView(title: "Tile GID", message: "\(tile)", delegate: nil, cancelButtonTitle: "Close")
         
-        alert_view.show()
+        // alert_view.show()
+        */
     }
     
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+        updateView()
     }
 }
